5bcbfa686efdf797e30a302253194078253889b5
Safin <safinghoghabori65@gmail.com>
2020-05-07 11:53:30 +0530

    Updates

---
+++ precache-manifest.3d4f9db0805e645bc0b02507017179b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872a1f1ac75345968cecc726bc84ca61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adfa716d1a5f6e05b8d9",
+    "url": "/static/css/main.b30d42d4.chunk.css"
+  },
+  {
+    "revision": "8e364649f4903f87c82e",
+    "url": "/static/js/2.cd58fb14.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cd58fb14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adfa716d1a5f6e05b8d9",
+    "url": "/static/js/main.331906fb.chunk.js"
+  },
+  {
+    "revision": "e8fa916aef6a9492070d",
+    "url": "/static/js/runtime-main.b2048a5b.js"
+  }
+]);
